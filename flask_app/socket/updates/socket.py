746448f801baaff60012ee2fa489a37b0c7a362c
Alen Alic <aalic89@gmail.com>
2020-09-22 14:53:00 +0200

    update => add more sockets (create and delete)

--- flask_app/socket/updates/socket.py
+++ flask_app/socket/updates/socket.py
@@ -1,6 +1,19 @@
 from flask_app.socket import socket_io, ROOM
 from flask_socketio import emit
 from models import Person, DancingClass, Couple
+from apis.person.apis import all_persons
+from apis.couple.apis import all_couples
+from apis.dancing_class.apis import all_dancing_classes
+
+
+@socket_io.on("person_created")
+def person_created():
+    emit(
+        "person_created",
+        all_persons(),
+        room=ROOM,
+        include_self=False
+    )
 
 
 @socket_io.on("person_updated")
@@ -9,6 +22,26 @@ def person_updated(person_id):
     emit(
         "person_updated",
         person.json(include_active_partners=True),
+        room=ROOM,
+        include_self=False
+    )
+
+
+@socket_io.on("person_deleted")
+def person_deleted(person_id):
+    emit(
+        "person_deleted",
+        person_id,
+        room=ROOM,
+        include_self=False
+    )
+
+
+@socket_io.on("couple_created")
+def couple_created():
+    emit(
+        "couple_created",
+        all_couples(),
         room=ROOM,
         include_self=False
     )
@@ -25,12 +58,42 @@ def couple_updated(couple_id):
     )
 
 
+@socket_io.on("couple_deleted")
+def couple_deleted(couple_id):
+    emit(
+        "couple_deleted",
+        couple_id,
+        room=ROOM,
+        include_self=False
+    )
+
+
+@socket_io.on("dancing_class_created")
+def dancing_class_created():
+    emit(
+        "dancing_class_created",
+        all_dancing_classes(),
+        room=ROOM,
+        include_self=False
+    )
+
+
 @socket_io.on("dancing_class_updated")
 def dancing_class_updated(dancing_class_id):
     dancing_class = DancingClass.query.filter(DancingClass.id == dancing_class_id).first()
     emit(
         "dancing_class_updated",
         dancing_class.json(),
+        room=ROOM,
+        include_self=False
+    )
+
+
+@socket_io.on("dancing_class_deleted")
+def dancing_class_deleted(dancing_class_id):
+    emit(
+        "dancing_class_deleted",
+        dancing_class_id,
         room=ROOM,
         include_self=False
     )
